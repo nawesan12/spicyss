4259de837bf0b47dc085ca78688489e24964ff1a
nawesan12 <nahueltecnicomdp@gmail.com>
2022-01-22 00:40:42 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Image src="/images/spicy.png" alt="SpicySS" width={150} height={150} />
         <h1>SpicySS</h1>
         <h3>Coming soon</h3>
       </main>
